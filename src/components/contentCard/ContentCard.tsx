15b18c969795a507fedfd220c8468ec8bb5c0e6b
Waritsara <warissara.0039@gmail.com>
2023-06-16 17:16:21 +0700

    fix eslint rule

--- src/components/contentCard/ContentCard.tsx
+++ src/components/contentCard/ContentCard.tsx
@@ -24,7 +24,7 @@ const ContentCard = ({ wallet, campaignName, button }: ContentCardProps) => {
       <div className="flex flex-col p-10 gap-5 w-full">
         <div className="">{campaignName}</div>
         <div>
-          description : Detail: Lorem Ipsum has been the industry's standard
+          description : Detail: Lorem Ipsum has been the industrys standard
           dummy text ever since the 1500s, when an unknown printer took a galley
           of type and scrambled it to make a type specimen book. It has survived
           not only five centuries, but also the leap into electronic
